ceefc1bfb9c5c0ac70310aefdf30e1840aff2af2
dudren-tree <dudrentree@gmail.com>
2021-04-22 19:09:46 +0300

    request fix

--- request.js
+++ request.js
@@ -1,23 +1,50 @@
 function sendRequest (method ,url) {
     return new Promise ((resolve, reject) => {
         const xhr = new XMLHttpRequest();
+        let splitUrl = url.split('//');
+        if(splitUrl[0] !== "https:") {
+            splitUrl.splice(0, 1 ,"https:");
+            let newUrl = splitUrl.join('//')
 
-        xhr.open(method, url);
+            console.log(newUrl)
 
-        xhr.responseType = 'json'
+            xhr.open(method, newUrl);
 
-        xhr.onload = () => {
-            if(xhr.status >= 400) {
-                reject(xhr.response)
-            } else {
-                resolve(xhr.response)
-            }      
-        }
+            xhr.responseType = 'json'
 
-        xhr.onerror = () => {
-            reject(xhr.response)
+            xhr.onload = () => {
+                if(xhr.status >= 400) {
+                    reject(xhr.response)
+                } else {
+                    resolve(xhr.response)
+                }      
             }
 
-        xhr.send()
+            xhr.onerror = () => {
+                reject(xhr.response)
+            }
+
+            xhr.send()
+        } else {
+            xhr.open(method, url);
+
+            xhr.responseType = 'json'
+
+            xhr.onload = () => {
+                if(xhr.status >= 400) {
+                    reject(xhr.response)
+                } else {
+                    resolve(xhr.response)
+                }      
+            }
+
+            xhr.onerror = () => {
+                reject(xhr.response)
+            }
+
+            xhr.send()
+        }
+
+        
     })
 }
